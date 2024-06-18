62e6f3f04290c01289503d962ef2654c85c51bbd
Muhammad Umar <mu8494759@gmail.com>
2024-06-18 20:15:37 +0500

    db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,16 +1,29 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose, { ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/whiper-box';
 
-export async function connectDB() : Promise<boolean>{
+// Define custom options extending ConnectOptions
+interface CustomConnectOptions extends ConnectOptions {
+    useNewUrlParser?: boolean;
+    useUnifiedTopology?: boolean;
+    useCreateIndex?: boolean;
+    useFindAndModify?: boolean;
+}
+
+export async function connectDB(): Promise<boolean> {
     try {
-        await mongoose.connect(uri);
-        console.log('database connected successfully.')
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        } as CustomConnectOptions);
+        console.log('Database connected successfully.');
         return true;
     } catch (error) {
-        console.log('failed to connect to database.');
+        console.error('Failed to connect to database.', error);
         return false;
     }
 }
