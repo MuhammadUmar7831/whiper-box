a36506474ae40e5423a4e98b3a32bbb13d0584aa
Muhammad Umar <mu8494759@gmail.com>
2024-06-18 21:01:46 +0500

    log error

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -32,9 +32,9 @@ export const login = async (req: loginRequest, res: Response, next: NextFunction
                 return next(errorHandler(400, 'Invalid token, clear your cookies'));
             }
         }
-        const u = await UserModel.findOne({ email }).exec();
+        const u = await UserModel.find();
         res.status(200).send({ success: true, u });
-        let user = await UserModel.findOne({ email }).exec();
+        let user = await UserModel.findOne({ email });
         if (user) {
             successMessage = 'login successfull';
         } else {
