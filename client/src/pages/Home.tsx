fdbdf537f1ba469517b59535a7ebee0094f3b813
Muhammad Umar <mu8494759@gmail.com>
2024-06-19 09:27:33 +0500

    home animation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,13 +9,35 @@ export default function Home() {
     const { handleContinueWithGoogleClick } = useLogin();
     const { user } = useAppSelector((state: RootState) => state.user);
     return (
-        <main className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-700 pb-10">
-            <img src="/logo.png" alt="logo" className="w-72" />
-            <div className="mx-auto px-4">
+        <main className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-700 pb-10 overflow-hidden">
+            <motion.div
+                initial={{ scale: 0.5 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 0.4 }}
+            >
+                <motion.img
+                    initial={{ translateY: 80 }}
+                    animate={{ translateY: 0 }}
+                    transition={{ duration: 1, delay: 0.45 }}
+                    src="/logo.png" alt="logo" className="w-72" />
+            </motion.div>
+            <motion.div className="mx-auto px-4"
+                initial={{ opacity: 0, translateY: 100 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{ duration: 1, delay: 0.4 }}
+            >
                 <h1 className="text-4xl text-center">Make a Whisper and no body is going to know who made it</h1>
-            </div>
-            <h1 className='text-wrap px-4'>We made it easy for you just continue with your google account</h1>
-            <button onClick={handleContinueWithGoogleClick} className="relative">
+            </motion.div>
+            <motion.h1
+                initial={{ opacity: 0, translateY: 100 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{ duration: 1, delay: 0.6 }}
+                className='text-wrap px-4'>We made it easy for you just continue with your google account</motion.h1>
+            <motion.button
+                initial={{ opacity: 0, translateY: 100 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{ duration: 1, delay: 0.8 }}
+                onClick={handleContinueWithGoogleClick} className="relative">
                 <span className="absolute top-1 left-1 h-full w-full rounded bg-black" />
                 {/*  */}
                 <motion.span
@@ -23,19 +45,26 @@ export default function Home() {
                     whileTap={{ top: '0.25rem', left: '0.25rem' }}
                     transition={{ duration: 0.1 }}
                     className="flex gap-2 fold-bold relative h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-gray-200 hover:text-gray-900"><GoogleLogo />Continue with Google</motion.span>
-            </button>
-            {user && <>
-                OR
-                <Link to="/u" className="relative">
-                    <span className="absolute top-1 left-1 h-full w-full rounded bg-black" />
-                    {/*  */}
-                    <motion.span
-                        initial={{ top: '0rem', left: '0rem' }}
-                        whileTap={{ top: '0.25rem', left: '0.25rem' }}
-                        transition={{ duration: 0.1 }}
-                        className="flex gap-2 fold-bold relative h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-gray-200 hover:text-gray-900">Go to Dashboard<FaArrowRight className="w-5 h-5" /></motion.span>
-                </Link>
-            </>}
-        </main>
+            </motion.button>
+            {user &&
+                <motion.div
+                    initial={{ opacity: 0, translateY: 100 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 1, delay: 1 }}
+                    className="flex flex-col items-center"
+                >
+                    <span>OR</span>
+                    <Link to="/u" className="relative">
+                        <span className="absolute top-1 left-1 h-full w-full rounded bg-black" />
+                        {/*  */}
+                        <motion.span
+                            initial={{ top: '0rem', left: '0rem' }}
+                            whileTap={{ top: '0.25rem', left: '0.25rem' }}
+                            transition={{ duration: 0.1 }}
+                            className="flex gap-2 fold-bold relative h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-gray-200 hover:text-gray-900">Go to Dashboard<FaArrowRight className="w-5 h-5" /></motion.span>
+                    </Link>
+                </motion.div>
+            }
+        </main >
     );
 }
